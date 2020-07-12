718faeaeefcbd18d33dfc230838049e45168255c
dhog10 <dhog10@googlemail.com>
2020-07-12 16:53:09 +0100

    Death screen

--- Assets/Scripts/UI/DeathScreen.cs
+++ Assets/Scripts/UI/DeathScreen.cs
@@ -11,6 +11,7 @@ public class DeathScreen : MonoBehaviour
 
     private float m_DeathTime;
     private bool m_Reloaded;
+    private float m_Alpha;
 
     void Update()
     {
@@ -21,7 +22,14 @@ public class DeathScreen : MonoBehaviour
 
         if (TankCharacterController.Instance.Alive && m_DeathScreenObject.activeSelf)
         {
-            m_DeathScreenObject.SetActive(false);
+            if (m_Alpha > 0f)
+            {
+                m_Alpha -= Time.deltaTime;
+            }
+            else
+            {
+                m_DeathScreenObject.SetActive(false);
+            }
         }
 
         if(!TankCharacterController.Instance.Alive && !m_DeathScreenObject.activeSelf)
@@ -33,21 +41,35 @@ public class DeathScreen : MonoBehaviour
 
         if (TankCharacterController.Instance.Alive)
         {
+            var color = m_DeathImage.color;
+            color.a = m_Alpha;
+            m_DeathImage.color = color;
+
+            var textColor = m_DeathText.color;
+            textColor.a = m_Alpha;
+            m_DeathText.color = textColor;
+
             return;
         }
-
-        var color = m_DeathImage.color;
-        color.a = Mathf.Clamp(m_DeathTime - 1 / 1f, 0f, 1f);
-        m_DeathImage.color = color;
-
-        var textColor = m_DeathText.color;
-        textColor.a = Mathf.Clamp(m_DeathTime - 2.5f / 1f, 0f, 1f);
-        m_DeathText.color = textColor;
-
-        if (!m_Reloaded && Time.time - m_DeathTime > 6f)
+        else
         {
-            m_Reloaded = true;
-            LevelManager.Instance.ReloadLevel();
+            var deathDiff = Time.time - m_DeathTime;
+
+            var color = m_DeathImage.color;
+            color.a = Mathf.Clamp(deathDiff - 3f / 1f, 0f, 1f);
+            m_DeathImage.color = color;
+
+            var textColor = m_DeathText.color;
+            textColor.a = Mathf.Clamp(deathDiff - 4.5f / 1f, 0f, 1f);
+            m_DeathText.color = textColor;
+
+            m_Alpha = Mathf.Max(m_Alpha, color.a);
+
+            if (!m_Reloaded && deathDiff > 9f)
+            {
+                m_Reloaded = true;
+                LevelManager.Instance.ReloadLevel();
+            }
         }
     }
 }
